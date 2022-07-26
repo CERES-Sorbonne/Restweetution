29848e27b89f6e58b7ab0e20dac9e4709c8112e2
David Gödicke <godicked@gmail.com>
2022-07-27 01:06:01 +0200

    fix import in elastic_view.py

--- restweetution/storages/__init__.py
+++
@@ -1,3 +0,0 @@
-from .elastic_storage import *
-from .object_storage import *
-from .postgres_storage import *
--- scripts/elastic_view.py
+++ scripts/elastic_view.py
@@ -3,7 +3,7 @@ import logging
 import os
 import restweetution.config as config
 from restweetution.data_view.data_view import ElasticView
-from restweetution.storages import ElasticStorage
+from restweetution.storages.elastic_storage.elastic_storage import ElasticStorage
 from restweetution.storages.postgres_storage.postgres_storage import PostgresStorage
 
 logging.basicConfig()
