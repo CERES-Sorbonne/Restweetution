6502dcb2eae2174d8b0fcc725c99477e0d92ebed
David Gödicke <godicked@gmail.com>
2022-07-26 20:00:39 +0200

    create new client after timeout inside twitter client Also use safe sessions inside other client functions

--- restweetution/twitter_client.py
+++ restweetution/twitter_client.py
@@ -2,38 +2,34 @@ import asyncio
 import logging
 import traceback
 from typing import Callable, List
-from urllib.parse import urljoin
 
 import aiohttp
 from aiohttp import ClientTimeout
 
 from restweetution.models.storage.stream_rule import RuleResponse
 
+default_timeout = ClientTimeout(total=300, sock_read=300, connect=300, sock_connect=300)
 
-class TwitterClient(aiohttp.ClientSession):
-    def __init__(self, token: str, base_url: str = "https://api.twitter.com/2/", error_handler: Callable = None):
-        super().__init__(timeout=ClientTimeout(total=300, sock_read=300, connect=300, sock_connect=300))
+
+class TwitterClient:
+    def __init__(self, token: str, base_url: str = "https://api.twitter.com", error_handler: Callable = None):
+        super().__init__()
         self.base_url = base_url
-        self.headers.update({"Authorization": f"Bearer {token}"})
+        self._headers = {"Authorization": f"Bearer {token}"}
         self._error_handler = error_handler
         self._logger = logging.getLogger("ApiClient")
+
+    def _get_client(self):
+        return aiohttp.ClientSession(headers=self._headers, timeout=default_timeout, base_url=self.base_url)
 
     def set_error_handler(self, error_handler: Callable):
         self._error_handler = error_handler
 
-    def _request(self, method, url, **kwargs):
-        modified_url = urljoin(self.base_url, url)
-        try:
-            res = super()._request(method, modified_url, **kwargs)
-            return res
-        except aiohttp.ClientResponseError as e:
-            self._error_handler(str(e), e.message)
-
     async def connect_tweet_stream(self, params, line_callback):
         while True:
             try:
-                async with self as session:
-                    async with session.get("https://api.twitter.com/2/tweets/search/stream", params=params) as resp:
+                async with self._get_client() as session:
+                    async with session.get("/2/tweets/search/stream", params=params) as resp:
                         async for line in resp.content:
                             asyncio.create_task(line_callback(line))
             except Exception as e:
@@ -41,28 +37,25 @@ class TwitterClient(aiohttp.ClientSession):
                 self._logger.exception(trace)
                 self._logger.exception(e)
 
-
     async def remove_rules(self, ids: List[str]):
         """
                 Remove rules by ids
                 :param ids: a list of ids of rules to remove
                 """
 
-        uri = "tweets/search/stream/rules"
-        async with self.post(uri, json={
-            "delete": {
-                "ids": ids
-            }
-        }) as r:
-            res = await r.json()
+        uri = "/2/tweets/search/stream/rules"
+        async with self._get_client() as session:
+            async with session.post(uri, json={"delete": {"ids": ids}}) as r:
+                res = await r.json()
 
-            # if everything went fine we return the ids of the deleted rules
-            if "errors" not in res:
-                self._logger.info(f'Removed {res["meta"]["summary"]["deleted"]} rule(s)')
-                return ids
+                # if everything went fine we return the ids of the deleted rules
+                if "errors" not in res:
+                    self._logger.info(f'Removed {res["meta"]["summary"]["deleted"]} rule(s)')
+                    return ids
 
-            # if not, return no ids as we can't know what rule failed or not
-            self._logger.error(res)
+                # if not, return no ids as we can't know what rule failed or not
+                self._logger.error(res)
+            # await session.close()
             return []
 
     async def get_rules(self, ids: List[str] = None):
@@ -73,28 +66,28 @@ class TwitterClient(aiohttp.ClientSession):
         :return: the list of rules
         """
 
-        uri = "tweets/search/stream/rules"
+        uri = "/2/tweets/search/stream/rules"
         if ids:
             uri += f"?ids={','.join(ids)}"
-        async with self.get(uri) as r:
-            res = await r.json()
-            if not res.get('data'):
-                res['data'] = []
-            res = RuleResponse(**res)
-            # print(res)
-            return res.data
+        async with self._get_client() as session:
+            async with session.get(uri) as r:
+                res = await r.json()
+                if not res.get('data'):
+                    res['data'] = []
+                res = RuleResponse(**res)
+                # print(res)
+                return res.data
 
     async def add_rules(self, rules):
-        uri = "tweets/search/stream/rules"
-        async with self.post(uri, json={
-            "add": rules
-        }) as r:
-            res = await r.json()
-            valid_rules = []
-            if 'errors' in res:
-                errs = res['errors']
-                for err in errs:
-                    self._logger.info(f"add_rules Error: {err['title']} Rule: {err['value']}")
-            if 'data' in res:
-                valid_rules = res['data']
-            return valid_rules
+        uri = "/2/tweets/search/stream/rules"
+        async with self._get_client() as session:
+            async with session.post(uri, json={"add": rules}) as r:
+                res = await r.json()
+                valid_rules = []
+                if 'errors' in res:
+                    errs = res['errors']
+                    for err in errs:
+                        self._logger.info(f"add_rules Error: {err['title']} Rule: {err['value']}")
+                if 'data' in res:
+                    valid_rules = res['data']
+                return valid_rules
