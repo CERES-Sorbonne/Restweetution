e377de527b332106c5b6a6896fdaf1d1de463791
david <godicked@gmail.com>
2023-04-20 17:20:56 +0200

    lookup script now detects why tweet is missing (deleted, protected, suspended)

--- scripts/lookup_tweets.py
+++ scripts/lookup_tweets.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 import os
+from collections import defaultdict
 from time import time
 
 import aiohttp
@@ -28,7 +29,11 @@ async def main():
     client = AsyncClient(bearer_token=token, return_type=dict, wait_on_rate_limit=True)
 
     query = CollectionQuery()
-    query.rule_ids = [71, 72, 15]
+    query.rule_ids = [47]
+
+    total = 0
+    total_found = 0
+    total_missing = 0
 
     async for data in storage.query_tweets_stream(query, chunk_size=100):
         tweet_ids = list(data.tweets.keys())
@@ -36,13 +41,58 @@ async def main():
         tweets = [Tweet(**d) for d in raw_res['data']]
         existing_ids = {t.id for t in tweets}
         missing_ids = set(tweet_ids) - existing_ids
+        user_to_check = defaultdict(list)
+        tweet_status = {t.id: 'alive' for t in tweets}
+        db_tweets = {t.id: t for t in data.get_tweets()}
+
+        if 'errors' in raw_res:
+            for error in raw_res['errors']:
+                tweet_id = error['resource_id']
+                if 'not-found' in error['type']:
+                    tweet_status[tweet_id] = "deleted"
+                else:
+                    tweet_status[tweet_id] = "unknown"
+                    user_to_check[db_tweets[tweet_id].author_id].append(tweet_id)
+        user_to_check_ids = list(user_to_check.keys())
+        if user_to_check_ids:
+            users_res = await client.get_users(ids=user_to_check_ids, user_fields=ALL_CONFIG.twitter_format()['user_fields'])
+
+            if 'data' in users_res:
+                for user in users_res['data']:
+                    user_id = user['id']
+                    if 'protected' in user and user['protected']:
+                        for t in user_to_check[user_id]:
+                            tweet_status[t] = 'protected'
+
+            if 'errors' in users_res:
+                for error in users_res['errors']:
+                    user_id = error['resource_id']
+                    detail = error['detail']
+                    if 'suspended' in detail:
+                        for t in user_to_check[user_id]:
+                            tweet_status[t] = 'suspended'
+                    else:
+                        for t in user_to_check[user_id]:
+                            tweet_status[t] = 'deleted_account'
 
         await storage.save_tweets(tweets)
 
         timestamp = datetime.datetime.now()
-        datas = [CustomData(id=t_id, key="missing", data={"deleted": True, "timestamp": timestamp}) for t_id in missing_ids]
-        if datas:
-            await storage.save_custom_datas(datas, override=False)
+        custom_key = "check_missing"
+        deleted_datas = [CustomData(id=t_id, key=custom_key, data={"deleted": True, "reason": tweet_status[t_id], "timestamp": timestamp}) for t_id in missing_ids]
+        if deleted_datas:
+            await storage.save_custom_datas(deleted_datas, override=False)
+
+        existing_datas = [CustomData(id=t_id, key=custom_key, data={"exist": True, "timestamp": timestamp}) for t_id in existing_ids]
+        if existing_datas:
+            await storage.save_custom_datas(existing_datas, override=True)
+
+        total += 100
+        total_found += len(existing_ids)
+        total_missing += len(missing_ids)
+        print(f'total: {total}  found[{total_found}]  missing[{total_missing}]')
+
+
 
 
 asyncio.run(main())
