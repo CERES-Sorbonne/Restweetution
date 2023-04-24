7ca9060da55d18ddb7e63899e61682065a34b649
david <godicked@gmail.com>
2023-04-24 22:11:22 +0200

    set try catch clause in tweet export task

--- restweetution/tasks/tweet_export_task.py
+++ restweetution/tasks/tweet_export_task.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 
 from restweetution import data_view
 from restweetution.data_view.view_exporter import ViewExporter
@@ -10,6 +11,8 @@ from restweetution.storages.exporter.exporter import Exporter, FileExporter
 from restweetution.storages.postgres_jsonb_storage.postgres_jsonb_storage import PostgresJSONBStorage
 from restweetution.tasks.server_task import ServerTask
 
+
+logger = logging.getLogger('ViewExportTask')
 
 class ViewExportTask(ServerTask):
     def __init__(self,
@@ -39,21 +42,25 @@ class ViewExportTask(ServerTask):
             raise ValueError(f'<<{self.view_type}>> view is not valid')
 
         async for res in storage_stream_function(self.query.query.collection):
-            coll = StorageCollection(self.storage, res)
-            # view specific steps
-            # save count of result
-            if self.view_type == ViewType.TWEET:
-                count = len(res.tweets)
-                await coll.load_all_from_tweets()
-            elif self.view_type == ViewType.MEDIA:
-                count = len(res.medias)
+            try:
+                coll = StorageCollection(self.storage, res)
+                # view specific steps
+                # save count of result
+                if self.view_type == ViewType.TWEET:
+                    count = len(res.tweets)
+                    await coll.load_all_from_tweets()
+                elif self.view_type == ViewType.MEDIA:
+                    count = len(res.medias)
 
-            view = coll.build_view(self.view_type, self.query.fields)
-            datas = [CustomData(key=self.key, id=d.id(), data=d) for d in view.view]
-            await self.exporter.save_custom_datas(datas)
+                view = coll.build_view(self.view_type, self.query.fields)
+                datas = [CustomData(key=self.key, id=d.id(), data=d) for d in view.view]
+                await self.exporter.save_custom_datas(datas)
 
-            self._progress += count
-            # await asyncio.sleep(0)
+                self._progress += count
+            except Exception as e:
+                logger.error(e)
+
+            await asyncio.sleep(0)
 
     def get_info(self):
         info = super().get_info()
